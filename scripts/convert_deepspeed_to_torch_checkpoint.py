36ce928a1c50e673cbe36262b4c7ed5de39e9bc8
Tony Lee <tonyh.lee@yahoo.com>
2022-03-21 17:38:19 -0700

    verify conversion went okay

--- scripts/convert_deepspeed_to_torch_checkpoint.py
+++ scripts/convert_deepspeed_to_torch_checkpoint.py
@@ -4,7 +4,7 @@ import os
 import torch
 
 """
-Convert DeepSpeed-format checkpoint to Torch-format checkpoint.
+Converts DeepSpeed-format checkpoint to Torch-format checkpoint.
 
 Example usage:
 
@@ -22,9 +22,13 @@ def convert_checkpoint(deepspeed_checkpoint_path: str, output_dir: str) -> str:
     Returns:
         Torch checkpoint path
     """
-    model_checkpoint = torch.load(deepspeed_checkpoint_path, map_location=torch.device('cpu'))
-    state_dict = model_checkpoint['module']
-    torch.nn.modules.utils.consume_prefix_in_state_dict_if_present(state_dict, prefix='module.')
+    model_checkpoint = torch.load(
+        deepspeed_checkpoint_path, map_location=torch.device("cpu")
+    )
+    state_dict = model_checkpoint["module"]
+    torch.nn.modules.utils.consume_prefix_in_state_dict_if_present(
+        state_dict, prefix="module."
+    )
 
     file_name: str = deepspeed_checkpoint_path.split(os.path.sep)[-1]
     output_path: str = os.path.join(output_dir, f"torch_{file_name}")
@@ -33,17 +37,17 @@ def convert_checkpoint(deepspeed_checkpoint_path: str, output_dir: str) -> str:
 
 
 def load_hf_model(torch_checkpoint_path: str):
-    """An example of how to load HuggingFace model from a Torch checkpoint."""
+    """
+    An example of how to load HuggingFace model from a Torch checkpoint.
+    """
     import transformers
 
     # Recreate your custom model...
-    config = transformers.GPT2Config(
-        n_positions=2048,
-    )
+    config = transformers.GPT2Config(n_positions=1024)
     model = transformers.AutoModelForCausalLM.from_config(config)
 
     # Read state dict from Pytorch checkpoint
-    state_dict = torch.load(torch_checkpoint_path, map_location=torch.device('cpu'))
+    state_dict = torch.load(torch_checkpoint_path, map_location=torch.device("cpu"))
 
     # Load FP16 weights, convert model to FP32 if desired
     model.load_state_dict(state_dict)
@@ -52,17 +56,20 @@ def load_hf_model(torch_checkpoint_path: str):
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--input-path", help="Path to DeepSpeed checkpoint")
     parser.add_argument(
-        "--input-path", help="Path to DeepSpeed checkpoint"
-    )
-    parser.add_argument(
-        "--output-dir", help="Path to output Torch checkpoint", default="."
+        "--output-dir", help="Output directory for the Torch checkpoint", default="."
     )
 
     args = parser.parse_args()
     output_path: str = convert_checkpoint(args.input_path, args.output_dir)
-    # load_hf_model(output_path)
-    print("Done.")
+
+    # Load HF model to verify the conversion went okay.
+    try:
+        load_hf_model(output_path)
+        print("Done and verified.")
+    except Exception as e:
+        print(f"Something went wrong: {str(e)}")
 
 
 if __name__ == "__main__":
