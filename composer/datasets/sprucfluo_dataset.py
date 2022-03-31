cfa12ebada4478ef42675eec25d02ad1ed8cec67
David Hall <dlwh@stanford.edu>
2022-03-31 12:46:39 -0700

    again!

--- composer/datasets/sprucfluo_dataset.py
+++ composer/datasets/sprucfluo_dataset.py
@@ -14,6 +14,7 @@ from composer.utils import dist
 
 from torchdata.datapipes.iter.util.samplemultiplexer import SampleMultiplexerDataPipe
 
+
 def _split_dict_fn(batch: Batch, n_microbatches: int) -> List[Batch]:
     if isinstance(batch, dict):
         chunked = {k: v.chunk(n_microbatches) for k, v in batch.items()}
@@ -50,118 +51,119 @@ class SprucfluoDatasetSpecHparams(hp.Hparams):
 
 @dataclass
 class SprucfluoDatasetHparams(DatasetHparams):
-  """
-      Builds a DataSpec that uses Sprucfluo for data processing.
+    """
+        Builds a DataSpec that uses Sprucfluo for data processing.
 
-      Parameters:
-          - datasets: List of SprucfluoDatasetSpecHparams
-          - weights: dict of [str, float] for weights (optional)
-  """
-  hparams_registry = {  # type: ignore
-      "datasets": {"dataset": SprucfluoDatasetSpecHparams},
-  }
-  datasets: List[SprucfluoDatasetSpecHparams] = hp.optional("list of SprucfluoDatasetSpec", default_factory=lambda: [])
-  weights: Optional[Dict[str, float]] = hp.optional("dict of [str, float] for weights", default=None)
+        Parameters:
+            - datasets: List of SprucfluoDatasetSpecHparams
+            - weights: dict of [str, float] for weights (optional)
+    """
+    hparams_registry = {  # type: ignore
+        "datasets": {"dataset": SprucfluoDatasetSpecHparams},
+    }
+    datasets: List[SprucfluoDatasetSpecHparams] = hp.optional("list of SprucfluoDatasetSpec",
+                                                              default_factory=lambda: [])
+    weights: Optional[Dict[str, float]] = hp.optional("dict of [str, float] for weights", default=None)
 
-  num_samples: int = hp.optional(
+    num_samples: int = hp.optional(
         "The number of post-processed token samples, used to set epoch size of the IterableDataset.", default=None)
-  tokenizer_name: str = hp.optional(
-      "The name of the HuggingFace tokenizer to preprocess text with.", default=None
-  )
-  max_seq_len: int = hp.optional(
-      "The max sequence length of each token sample.", default=1024
-  )
-  shuffle: bool = hp.optional(
-      "Whether to shuffle the samples in the dataset. Currently, shards are assigned and consumed with deterministic "
-      "per-device shard order, but shuffling affects the order of samples via (per-device) shuffle buffers.",
-      default=True,
-  )
-  shuffle_buffer_size: int = hp.optional(
-      "If `shuffle=True`, samples are read into a buffer of this size (per-device), and randomly sampled from there "
-      "to produce shuffled samples.",
-      default=10000,
-  )
-  seed: int = hp.optional(
-      "If `shuffle=True`, what seed to use for shuffling operations.", default=5
-  )
-  drop_last: bool = hp.optional(
-      "Whether to drop the last samples for the last batch.", default=True
-  )
+    tokenizer_name: str = hp.optional(
+        "The name of the HuggingFace tokenizer to preprocess text with.", default=None
+    )
+    max_seq_len: int = hp.optional(
+        "The max sequence length of each token sample.", default=1024
+    )
+    shuffle: bool = hp.optional(
+        "Whether to shuffle the samples in the dataset. Currently, shards are assigned and consumed with deterministic "
+        "per-device shard order, but shuffling affects the order of samples via (per-device) shuffle buffers.",
+        default=True,
+    )
+    shuffle_buffer_size: int = hp.optional(
+        "If `shuffle=True`, samples are read into a buffer of this size (per-device), and randomly sampled from there "
+        "to produce shuffled samples.",
+        default=10000,
+    )
+    seed: int = hp.optional(
+        "If `shuffle=True`, what seed to use for shuffling operations.", default=5
+    )
+    drop_last: bool = hp.optional(
+        "Whether to drop the last samples for the last batch.", default=True
+    )
 
-  def validate(self):
-      assert len(self.datasets) > 0, "datasets must be a list of SprucfluoDatasetSpec"
-      assert len(list(d.name for d in self.datasets)) == len(set(d.name for d in self.datasets)), "datasets must have unique names"
+    def validate(self):
+        assert len(self.datasets) > 0, "datasets must be a list of SprucfluoDatasetSpec"
+        assert len(list(d.name for d in self.datasets)) == len(
+            set(d.name for d in self.datasets)), "datasets must have unique names"
 
-      if self.weights is not None:
-          assert len(self.weights) == len(self.datasets), "weights must be a dict of [str, float] for weights"
-          for dataset in self.datasets:
-              assert dataset.name in self.weights, f"{dataset.name} not in weights"
+        if self.weights is not None:
+            assert len(self.weights) == len(self.datasets), "weights must be a dict of [str, float] for weights"
+            for dataset in self.datasets:
+                assert dataset.name in self.weights, f"{dataset.name} not in weights"
 
+    def initialize_object(self, batch_size: int, dataloader_hparams: DataLoaderHparams) -> DataSpec:
+        try:
+            import transformers
+        except ImportError:
+            raise ImportError('HuggingFace transformers not installed. '
+                              'Please install with `pip install composer[nlp]`')
 
+        if dataloader_hparams.num_workers > 1:
+            log.warning("Sprucfluo Dataset not compatible with num_workers > 1. Overwriting value to num_workers=1")
+            dataloader_hparams.num_workers = 1
 
-  def initialize_object(self, batch_size:int, dataloader_hparams: DataLoaderHparams) -> DataSpec:
-      try:
-          import transformers
-      except ImportError:
-          raise ImportError('HuggingFace transformers not installed. '
-                            'Please install with `pip install composer[nlp]`')
+        datasets = {d.name: d.initialize_object() for d in self.datasets}
 
-      if dataloader_hparams.num_workers > 1:
-          log.warning("Sprucfluo Dataset not compatible with num_workers > 1. Overwriting value to num_workers=1")
-          dataloader_hparams.num_workers = 1
+        world_size = dist.get_world_size()
+        num_samples_per_device = self.num_samples // world_size
+        if self.num_samples % world_size != 0:
+            new_num_samples = num_samples_per_device * world_size
+            log.warning(
+                f"Num samples will be truncated from {num_samples}->{new_num_samples} to maintain divisibility "
+                f"across {world_size} devices."
+            )
+            self.num_samples = new_num_samples
 
-      datasets = {d.name: d.initialize_object() for d in self.datasets}
+        # Build tokenizer
+        tokenizer = transformers.AutoTokenizer.from_pretrained(self.tokenizer_name)
+        if tokenizer.pad_token is None:
+            # Some tokenizers (e.g. GPT2 tokenizer) have no padding token which causes bugs
+            tokenizer.pad_token = tokenizer.eos_token
 
-      world_size = dist.get_world_size()
-      num_samples_per_device = self.num_samples // world_size
-      if self.num_samples % world_size != 0:
-          new_num_samples = num_samples_per_device * world_size
-          log.warning(
-              f"Num samples will be truncated from {num_samples}->{new_num_samples} to maintain divisibility "
-              f"across {world_size} devices."
-          )
-          self.num_samples = new_num_samples
+        process_fun = functools.partial(sprucfluo.tokenize_and_group_texts, tokenizer=tokenizer,
+                                        seq_len=self.max_seq_len)
 
-      # Build tokenizer
-      tokenizer = transformers.AutoTokenizer.from_pretrained(self.tokenizer_name)
-      if tokenizer.pad_token is None:
-          # Some tokenizers (e.g. GPT2 tokenizer) have no padding token which causes bugs
-          tokenizer.pad_token = tokenizer.eos_token
+        datasets = {k: dataset.then(process_fun) for k, dataset in datasets.items()}
 
-      process_fun = functools.partial(sprucfluo.tokenize_and_group_texts, tokenizer=tokenizer, seq_len=self.max_seq_len)
+        if len(datasets) == 1:
+            dataset = next(iter(datasets.values()))
+        else:
+            weights = self.weights
+            if weights is None:
+                weights = {d.name: 1.0 for d in self.datasets}
+            weights = {datasets[d.name]: weights[d.name] for d in self.datasets if weights[d.name] > 0}
+            dataset = SampleMultiplexerDataPipe(weights, seed=self.seed)
 
-      datasets = {k: dataset.then(process_fun) for k, dataset in datasets.items()}
+        if self.shuffle:
+            dataset = dataset.shuffle(buffer_size=self.shuffle_buffer_size)
 
-      if len(datasets) == 1:
-          dataset = next(iter(datasets.values()))
-      else:
-          weights = self.weights
-          if weights is None:
-              weights = {d.name: 1.0 for d in self.datasets}
-          weights = {datasets[d.name]: weights[d.name] for d in self.datasets if weights[d.name] > 0}
-          dataset = SampleMultiplexerDataPipe(weights, seed=self.seed)
+        dataset = _AssumeLenDataset(dataset, num_samples_per_device)
 
-      if self.shuffle:
-          dataset = dataset.shuffle(buffer_size=self.shuffle_buffer_size)
-
-      dataset = _AssumeLenDataset(dataset, num_samples_per_device)
-
-      # Get collate_fn
-      collate_fn = transformers.DataCollatorForLanguageModeling(
-          tokenizer=tokenizer,
-          mlm=False,
-      )
-      # Return DataSpec
-      return DataSpec(
-          dataloader=dataloader_hparams.initialize_object(
-              dataset=dataset,
-              batch_size=batch_size,
-              sampler=None,
-              drop_last=True,
-              collate_fn=collate_fn,
-          ),
-          split_batch=_split_dict_fn,
-      )
+        # Get collate_fn
+        collate_fn = transformers.DataCollatorForLanguageModeling(
+            tokenizer=tokenizer,
+            mlm=False,
+        )
+        # Return DataSpec
+        return DataSpec(
+            dataloader=dataloader_hparams.initialize_object(
+                dataset=dataset,
+                batch_size=batch_size,
+                sampler=None,
+                drop_last=True,
+                collate_fn=collate_fn,
+            ),
+            split_batch=_split_dict_fn,
+        )
 
 
 class _AssumeLenDataset(torch.utils.data.dataset.IterableDataset):
