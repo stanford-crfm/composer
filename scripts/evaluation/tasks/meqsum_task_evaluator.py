fa8f12fa3534bfea1f52aa08402589f3d199f1ae
J38 <jebolton@stanford.edu>
2022-04-27 05:29:44 -0700

    task evaluator for MeQSum

---
+++ scripts/evaluation/tasks/meqsum_task_evaluator.py
@@ -0,0 +1,90 @@
+import os
+from wandb.apis.public import Run, Artifact
+
+from scripts.evaluation.evaluator_state import EvaluatorState
+from .downstream_task_evaluator import DownstreamTaskEvaluator
+
+
+class MeQSumTaskEvaluator(DownstreamTaskEvaluator):
+    def __init__(
+        self,
+        evaluator_state: EvaluatorState,
+        run: Run,
+        artifact: Artifact,
+        step: int,
+        downstream_dir_path: str,
+        checkpoint_path: str,
+    ):
+        super().__init__(
+            evaluator_state, run, artifact, step, downstream_dir_path, checkpoint_path
+        )
+
+    @property
+    def task_name(self) -> str:
+        # TODO: add hyperparameter values to the name
+        return "MeQSum"
+
+    @property
+    def command(self) -> str:
+        task_name: str = "MeQSum"
+        uid: str = f"{self.run.name}-{self.artifact.name}"
+        finetune_executable: str = "train_e2e.py"
+        gen_executable: str = "gen_batch.py"
+        data_dir: str = os.path.join("data", f"{task_name}")
+
+        # TODO: Hardcoded to default parameters for now. We can make this configurable too.
+        # TODO: add --fp16
+        finetune_cmd: str = " ".join(
+            [
+                "python",
+                "-u",
+                finetune_executable,
+                "--mode",
+                f"{task_name}",
+                "--tuning_mode",
+                "finetune",
+                "--epoch",
+                "100",
+                "--learning_rate",
+                "1e-4",
+                "--bsz",
+                "16",
+                "--gradient_accumulation_step",
+                "2",
+                "--seed",
+                "101",
+                "--model_name_or_path",
+                f"{self.checkpoint_path}",
+                "--dir_name",
+                uid,
+                "--warmup_steps",
+                "1000",
+            ]
+        )
+        gen_cmd: str = " ".join(
+            [
+                "python",
+                "-u",
+                gen_executable,
+                "--mode",
+                f"{task_name}",
+                "--batch_size",
+                "9",
+                "--length",
+                "400",
+                "--no_repeat_ngram_size",
+                "6",
+                "--control_mode",
+                "no",
+                "--use_prefixtuning",
+                "0",
+                "--eval_split",
+                "test",
+                "--base_model_name_or_path",
+                f"{self.checkpoint_path}",
+                "--load_checkpoint_path",
+                os.path.join(f"runs_{task_name}", uid)
+            ]
+        )
+        command: str = " ; ".join([finetune_cmd, gen_cmd])
+        return command
