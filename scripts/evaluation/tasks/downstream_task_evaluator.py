459359b0177b83db9072ee0eea4a0b37c7ae5d64
J38 <jebolton@stanford.edu>
2022-05-05 14:46:20 -0700

    fix bugs

--- scripts/evaluation/tasks/downstream_task_evaluator.py
+++ scripts/evaluation/tasks/downstream_task_evaluator.py
@@ -23,8 +23,9 @@ class DownstreamTaskEvaluator(ABC):
         self.artifact: Artifact = artifact
         self.step: int = step
         self.evaluator_state: EvaluatorState = evaluator_state
+        self.downstream_config = downstream_config
         # generate all requested hparam settings for the task
-        self.hparams: dict = dict(downstream_config)
+        self.hparams: dict = dict(downstream_config["hparams"])
         for k in ["environment", "path"]:
             self.hparams.pop(k, None)
         # task params are keyed with 2 attributes
@@ -45,7 +46,7 @@ class DownstreamTaskEvaluator(ABC):
         self.original_work_dir: str = os.path.abspath(os.getcwd())
         self.checkpoint_path: str = os.path.abspath(checkpoint_path)
         self.results_dir: str = os.path.abspath(
-            os.path.join(self.checkpoint_path, "result", self.task_name)
+            os.path.join(self.checkpoint_path, "result", self.task_base_name)
         )
         os.makedirs(self.results_dir, exist_ok=True)
 
@@ -56,7 +57,7 @@ class DownstreamTaskEvaluator(ABC):
         k_prefix: str = " ",
         v_prefix: str = " ",
         ignore_keys: list = [],
-        sort_keys=True
+        sort_keys=True,
     ) -> str:
         """
         Turn a config dict into a str. Add prefixes to keys and values, potentially ignore keys.
@@ -81,18 +82,21 @@ class DownstreamTaskEvaluator(ABC):
         config_copy = dict(config)
         for k in config_copy:
             if type(config_copy[k]) != list:
-                config_copy[k] = list[config_copy[k]]
-        return [dict(zip(config.keys(), value_combo)) for value_combo in itertools.product(*config_copy.values())]
-
+                config_copy[k] = [config_copy[k]]
+        return [
+            dict(zip(config.keys(), value_combo))
+            for value_combo in itertools.product(*config_copy.values())
+        ]
 
     def task_name(self, config) -> str:
-        options_label = config_to_str(config=config,delimiter="_",k_prefix="",v_prefix="=")
+        options_label = DownstreamTaskEvaluator.config_to_str(
+            config=config, delimiter="_", k_prefix="", v_prefix="="
+        )
         return f"{self.task_base_name}_{options_label}"
 
     @abstractmethod
     def command(self, config) -> str:
         pass
-
 
     def build_command(self, executables: list, defaults: dict, config: dict):
         """
@@ -102,10 +106,14 @@ class DownstreamTaskEvaluator(ABC):
         sub_cmds = []
         for executable in executables:
             executable_config = dict(defaults[executable])
-            executable_config.update(config)
-            sub_cmds.append(DownstreamTaskEvaluator.config_to_str(config=executable_config,delimiter=" ",k_prefix="--",v_prefix=" "))
-        return ";".join(sub_cmds) 
-
+            executable_config.update(config[executable])
+            sub_cmds.append(
+                f"python -u {executable} "
+                + DownstreamTaskEvaluator.config_to_str(
+                    config=executable_config, delimiter=" ", k_prefix="--", v_prefix=" "
+                )
+            )
+        return " ; ".join(sub_cmds)
 
     def run_name(self, config) -> str:
         return f"{self.run.name}_step={self.step}_task={self.task_name(config)}"
@@ -137,16 +145,22 @@ class DownstreamTaskEvaluator(ABC):
                 else:
                     model_type_config = None
                 if model_type_config:
-                    cmd_config_combos = DownstreamTaskEvaluator.config_cartesian(self.hparams[model_type_config])
+                    cmd_config_combos = DownstreamTaskEvaluator.config_cartesian(
+                        self.hparams[model_type_config]
+                    )
                     for cmd_config in cmd_config_combos:
-                        hparam_cmd = self.command(config=hparam_config)
+                        hparam_cmd = self.command(config=cmd_config)
                         if self.environment:
                             # determine location of conda.sh
-                            conda_prefix = os.environ["CONDA_EXE"][:-len("/bin/conda")]
+                            conda_prefix = os.environ["CONDA_EXE"][: -len("/bin/conda")]
                             conda_shell = f"{conda_prefix}/etc/profile.d/conda.sh"
-                            assert os.path.exists(conda_shell), f"Error: {conda_shell} does not exist"
+                            assert os.path.exists(
+                                conda_shell
+                            ), f"Error: {conda_shell} does not exist"
                             # add conda set up to task command
-                            setup_env_cmd = ". {conda_shell} ; conda activate {self.environment}"
+                            setup_env_cmd = (
+                                f". {conda_shell} ; conda activate {self.environment}"
+                            )
                             hparam_cmd = f"{setup_env_cmd} ; {hparam_cmd}"
                         hlog(hparam_cmd)
                         subprocess.check_call(hparam_cmd, shell=True)
