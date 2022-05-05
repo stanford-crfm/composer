a0342bad4b6b6be305b6c697cb270b8bc18fda31
J38 <jebolton@stanford.edu>
2022-05-05 06:33:32 -0700

    update to new api

--- scripts/evaluation/tasks/medparasimp_task_evaluator.py
+++ scripts/evaluation/tasks/medparasimp_task_evaluator.py
@@ -7,6 +7,8 @@ from .downstream_task_evaluator import DownstreamTaskEvaluator
 
 class MedParaSimpTaskEvaluator(DownstreamTaskEvaluator):
 
+    task_base_name = "MedParaSimp"
+
     def __init__(
         self,
         evaluator_state: EvaluatorState,
@@ -20,71 +22,38 @@ class MedParaSimpTaskEvaluator(DownstreamTaskEvaluator):
             evaluator_state, run, artifact, step, downstream_dir_path, checkpoint_path
         )
 
-    @property
-    def task_name(self) -> str:
-        # TODO: add hyperparameter values to the name
-        return "MedParaSimp"
 
     def command(self, config=None) -> str:
-        task_name: str = "medparasimp"
+        gen_task_name = "medparasimp"
         uid: str = f"{self.run.name}-{self.artifact.name}"
-        finetune_executable: str = "train_e2e.py"
-        gen_executable: str = "gen_batch.py"
-        data_dir: str = os.path.join("data", f"{task_name}")
-
-        # TODO: Hardcoded to default parameters for now. We can make this configurable too.
-        # TODO: add --fp16
-        finetune_cmd: str = " ".join(
-            [
-                "python",
-                "-u",
-                finetune_executable,
-                "--mode",
-                f"{task_name}",
-                "--tuning_mode",
-                "finetune",
-                "--epoch",
-                "100",
-                "--learning_rate",
-                "1e-4",
-                "--bsz",
-                "16",
-                "--gradient_accumulation_step",
-                "2",
-                "--seed",
-                "101",
-                "--model_name_or_path",
-                f"{self.checkpoint_path}",
-                "--dir_name",
-                uid,
-                "--warmup_steps",
-                "1000",
-            ]
-        )
-        gen_cmd: str = " ".join(
-            [
-                "python",
-                "-u",
-                gen_executable,
-                "--mode",
-                f"{task_name}",
-                "--batch_size",
-                "9",
-                "--length",
-                "400",
-                "--no_repeat_ngram_size",
-                "6",
-                "--control_mode",
-                "no",
-                "--use_prefixtuning",
-                "0",
-                "--eval_split",
-                "test",
-                "--base_model_name_or_path",
-                f"{self.checkpoint_path}",
-                "--load_checkpoint_path",
-                os.path.join(f"runs_{task_name}", uid)
-            ]
-        )
-        command: str = " ; ".join([finetune_cmd, gen_cmd])
-        return command
+        executables: list = ["train_e2e.py", "gen_batch.py"]
+        defaults: dict = {
+            "train_e2e.py": {
+                "mode": gen_task_name,
+                "tuning_mode": "finetune",
+                "epoch": 100,
+                "learning_rate": 1e-4,
+                "bsz": 16,
+                "gradient_accumulation_step": 2,
+                "seed": 101,
+                "model_name_or_path": self.checkpoint_path,
+                "dir_name": uid,
+                "warmup_steps": 1000
+            },
+            "gen_batch.py": {
+                "mode": "medparasimp",
+                "batch_size": 9,
+                "length": 400,
+                "no_repeat_ngram_size": 6,
+                "control_mode": "no",
+                "use_prefixtuning": 0,
+                "eval_split": "test",
+                "base_model_name_or_path": self.checkpoint_path,
+                "load_checkpoint_path": os.path.join(f"runs_{gen_task_name}", uid),
+                "wandb_entity": self.downstream_config["wandb_entity"],
+                "wandb_project": self.downstream_config["wandb_project"],
+                "wandb_run_name": self.run_name(config)
+           }
+        }
+        self.build_command(executables=executables,defaults=defaults,config=config)
+        
