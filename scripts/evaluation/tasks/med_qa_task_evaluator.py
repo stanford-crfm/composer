4555f28ae5bdd5a152bab69220cef32bcbd9c01c
J38 <jebolton@stanford.edu>
2022-05-18 02:12:02 -0700

    update to new format

--- scripts/evaluation/tasks/med_qa_task_evaluator.py
+++ scripts/evaluation/tasks/med_qa_task_evaluator.py
@@ -5,7 +5,7 @@ from scripts.evaluation.evaluator_state import EvaluatorState
 from .downstream_task_evaluator import DownstreamTaskEvaluator
 
 
-class MedQATaskEvaluator(DownstreamTaskEvaluator):
+class PubMedQATaskEvaluator(DownstreamTaskEvaluator):
 
     task_base_name = "MedQA"
 
@@ -22,44 +22,37 @@ class MedQATaskEvaluator(DownstreamTaskEvaluator):
             evaluator_state, run, artifact, step, downstream_config, checkpoint_path
         )
 
-    @property
-    def task_name(self) -> str:
-        # TODO: add hyperparameter values to the name
-        return "MedQA"
 
-    @property
-    def command(self) -> str:
-        executable: str = "run_multiple_choice.py"
+    def command(self, config: dict={}) -> str:
+        qa_task_name = "medqa_usmle_hf"
+        executables: list = ["run_multiple_choice.py"]
         data_dir: str = os.path.join("data", "medqa_usmle_hf")
+        defaults: dict = {
+            "run_multiple_choice.py": {
+                "tokenizer_name": "gpt2",
+                "model_name_or_path": self.checkpoint_path,
+                "train_file": f"{data_dir}/train.json",
+                "validation_file": f"{data_dir}/dev.json",
+                "test_file": f"{data_dir}/test.json",
+                "do_train": "",
+                "do_eval": "",
+                "do_predict": "",
+                "fp16": "",
+                "per_device_train_batch_size": "1",
+                "per_device_eval_batch_size": "1",
+                "gradient_accumulation_steps": "32",
+                "learning_rate": "3e-5",
+                "warmup_ratio": "0.5",
+                "num_train_epochs": "30",
+                "max_seq_length": "512",
+                "logging_first_step": "",
+                "logging_steps": "20",
+                "save_strategy": "no",
+                "evaluation_strategy": "steps",
+                "eval_steps": "100",
+                "run_name": self.run_name(config["run_multiple_choice.py"]),
+                "output_dir": f"{self.results_dir}",
+            }
+        }
+        return self.build_command(executables=executables,defaults=defaults,config=config)
 
-        # TODO: Hardcoded to default parameters for now. We can make this configurable too.
-        # TODO: add --fp16
-        command: str = " ".join(
-            [
-                "python3",
-                executable,
-                "--tokenizer_name gpt2",
-                f"--model_name_or_path {self.checkpoint_path}",
-                f"--train_file {data_dir}/train.json",
-                f"--validation_file {data_dir}/dev.json",
-                f"--test_file {data_dir}/test.json",
-                "--do_train",
-                "--do_eval",
-                "--do_predict",
-                "--per_device_train_batch_size 1",
-                "--per_device_eval_batch_size 1",
-                "--gradient_accumulation_steps 32",
-                "--learning_rate 3e-5",
-                "--warmup_ratio 0.5",
-                "--num_train_epochs 30",
-                "--max_seq_length 512",
-                "--logging_first_step",
-                "--logging_steps 20",
-                "--save_strategy no",
-                "--evaluation_strategy steps",
-                "--eval_steps 100",
-                f"--run_name {self.run_name}",
-                f"--output_dir {self.results_dir}",
-            ]
-        )
-        return command
